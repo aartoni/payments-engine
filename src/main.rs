ab46b65184238dc541d3f7115b5d04429ffb3855
aartoni <arto7796@gmail.com>
2022-05-25 22:43:56 -0400

    Add deposit transactions processing

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::{error::Error, fs::File, env};
 
 use csv::ReaderBuilder;
-use payments::transaction::Transaction;
+use payments::{transaction::Transaction, payments_engine::PaymentsEngine};
 
 fn main() -> Result<(), Box<dyn Error>> {
     // Get the CSV reader
@@ -13,10 +13,13 @@ fn main() -> Result<(), Box<dyn Error>> {
         .comment(Some(b'#'))
         .from_reader(&file);
 
+    // Create a payments engine
+    let mut engine = PaymentsEngine::new();
+
     // Parse each line and perform the transaction
     for result in reader.deserialize() {
         let transaction: Transaction = result?;
-        // ...
+        engine.execute(transaction);
     }
 
     // Print each client data
